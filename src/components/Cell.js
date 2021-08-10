3c4228e2a84506fdde09091b1d2d55fa42d57769
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-08-10 17:27:26 -0400

    Controls cell sizing

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-  <div>cell</div>
-)
+  <StyledCell type={"J"} color={TETROMINOS[type].color} />
+);
 
-export default Cell;
+export default React.memo(Cell);
